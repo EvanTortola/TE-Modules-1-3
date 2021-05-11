ecbe49958a4f4ea20d79ee2d296d8344c7360ebb
Evan Tortola <etortola58@gmail.com>
2021-05-11 12:43:47 -0400

    Started working on day 2

--- module-1/02_Variables_Data_Types/exercise-student/java/src/main/java/com/techelevator/Exercises.java
+++ module-1/02_Variables_Data_Types/exercise-student/java/src/main/java/com/techelevator/Exercises.java
@@ -26,6 +26,9 @@ public class Exercises {
         3. 3 raccoons are playing in the woods. 2 go home to eat dinner. How
         many raccoons are left in the woods?
         */
+		int racoonsPlaying = 3;
+		int racoonsLeave = 2;
+		int racoonsRemaining - racoonsPlaying - racoonsLeave;
 
 
         /* Exercise 4
--- module-1/02_Variables_Data_Types/lecture-student/java/src/main/java/com/techelevator/Lecture.java
+++ module-1/02_Variables_Data_Types/lecture-student/java/src/main/java/com/techelevator/Lecture.java
@@ -9,6 +9,13 @@ public class Lecture {
 			Then set it to 26.
 		*/
 
+//		int numberOfExercises;
+//		numberOfExercises = 26;
+
+		int numberOfExercises = 26;
+
+		System.out.println(numberOfExercises);
+
 		//System.out.println(numberOfExercises);
 
 		/*
@@ -16,6 +23,8 @@ public class Lecture {
 			Set it to 0.5.
 		*/
 
+		double half = 0.5;
+		System.out.println(half);
 		//System.out.println(half);
 
 		/*
@@ -23,110 +32,138 @@ public class Lecture {
 			Set it to "TechElevator".
 		*/
 
+		String name = "TechElevator";
+		System.out.println(name);
 		//System.out.println(name);
 
 		/*
 		4. Create a variable called seasonsOfFirefly and set it to 1.
 		*/
 
-		//System.out.println(seasonsOfFirefly);
+		int seasonsOfFirefly = 1;
+		System.out.println(seasonsOfFirefly);
 
 		/*
 		5. Create a variable called myFavoriteLanguage and set it to "Java".
 		*/
-
-		//System.out.println(myFavoriteLanguage);
+		String myFavoriteLanguage = "Java";
+		System.out.println(myFavoriteLanguage);
 
 		/*
 		6. Create a variable called pi and set it to 3.1416.
 		*/
-
-		//System.out.println(pi);
+		float pi = 3.1416f;
+		System.out.println(pi);
 
 		/*
 		7. Create and set a variable that holds your name.
 		*/
+		String myName = "Evan";
 
 		/*
 		8. Create and set a variable that holds the number of buttons on your mouse.
 		*/
+		byte numberOfMouseButtons = 5;
 
 		/*
 		9. Create and set a variable that holds the percentage of battery left on
 		your phone.
 		*/
+		float batteryPctRemaining;
 
 		/*
 		10. Create an int variable that holds the difference between 121 and 27.
 		*/
-
+		int result = 121 - 27;
 		/*
 		11. Create a double that holds the addition of 12.3 and 32.1.
 		*/
-
+		double result11 = 12.3 + 32.1;
 		/*
 		12. Create a String that holds your full name.
 		*/
-
+		String fullName = "Evan Tortola";
+		System.out.println(fullName);
 		/*
 		13. Create a String that holds the word "Hello, " concatenated onto your
 		name from above.
 		*/
+		String helloMe = "Hello, " + fullName;
+		System.out.println(helloMe);
 
 		/*
 		14. Add a " Esquire" onto the end of your full name and save it back to
 		the same variable.
 		*/
+		fullName = fullName + " Esquire";
 
 		/*
 		15. Now do the same as exercise 14, but use the += operator.
 		*/
-
+		fullName += " Esquire";
 		/*
 		16. Create a variable to hold "Saw" and add a 2 onto the end of it.
 		*/
-
+		String movieName = "Saw" + 2;
+		System.out.println(movieName);
 		/*
 		17. Add a 0 onto the end of the variable from exercise 16.
 		*/
+		movieName = movieName + 0;
+		System.out.println(movieName);
 
 		/*
 		18. What is 4.4 divided by 2.2?
 		*/
-
+		System.out.println(4.4 / 2.2);
 		/*
 		19. What is 5.4 divided by 2?
 		*/
+		System.out.println(5.4 / 2);
 
 		/*
 		20. What is 5 divided by 2?
 		*/
+		System.out.println(5 / 2);
+
 
 		/*
 		21. What is 5.0 divided by 2?
 		*/
-
+		System.out.println(5.0 / 2);
 		/*
 		22. What is 66.6 divided by 100? Is the answer you get right or wrong?
 		*/
+		System.out.println(66.6 / 100);
 
 		/*
 		23. If I divide 5 by 2, what's my remainder?
 		*/
+		System.out.println( 5 % 2 );
 
 		/*
 		24. What is 1,000,000,000 * 3?
 		*/
+		System.out.println(1000000000 * 3 );
+
+		byte overflowed = (byte) 127;
+		overflowed += 1;
+		System.out.println(overflowed);
+		System.out.println(overflowed + 1);
+		System.out.println(overflowed + 2);
 
 		/*
 		25. Create a variable that holds a boolean called doneWithExercises and
 		set it to false.
 		*/
 
+		boolean doneWithExercises = false;
+
 		/*
 		26. Now set doneWithExercise to true.
 		*/
-		
+
+		doneWithExercises = true;
 	}
 
 }
