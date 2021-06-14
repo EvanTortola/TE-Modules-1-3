5b4687015ed625a75a0be6cc63c38af5aeb55f2b
Evan Tortola <etortola58@gmail.com>
2021-06-14 10:38:25 -0400

    Exercise 24 6/14

--- module-2/24_INSERT_UPDATE_and_DELETE/exercise-student/postgres/Exercises/04_add_genre.sql
+++ module-2/24_INSERT_UPDATE_and_DELETE/exercise-student/postgres/Exercises/04_add_genre.sql
@@ -1,11 +1,14 @@
 -- 4. Add a "Sports" genre to the genre table. Add the movie "Coach Carter" to the newly created genre. (1 row each)
 
-
-
+START TRANSACTION;
 
 INSERT INTO genre (genre_name)
-VALUES ('Sports');
+VALUES ('Sports')
 
-INSERT INTO movie_genre (movie_id, genre_id)
-VALUES ((SELECT movie_id FROM movie WHERE title = 'Coach Carter'),
-         (SELECT genre_id FROM genre WHERE genre_name = 'Sports'));
+
+
+INSERT INTO movie (title)
+VALUES ('Coach Carter')
+WHERE (SELECT genre_name FROM genre WHERE genre_name = 'Sports');
+ROLLBACK;
+COMMIT;
